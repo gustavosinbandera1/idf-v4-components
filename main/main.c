2e9eb4943ab33dff9099b996ab8c4a203f13c483
gustavosinbandera1 <gustavosinbandera1@hotmail.com>
2019-12-16 15:56:26 -0500

    first commit

---
+++ main/main.c
@@ -0,0 +1,46 @@
+
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+
+
+#include "my_print1.h"
+#include "my_print2.h"
+
+//////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
+
+void app_main(void)
+{
+
+
+	while(1)
+	{
+
+	    for (int i = 10; i >= 0; i--) 
+		{
+    	           		
+			myPrintf1( i );
+       		
+       		myPrintf2( i );
+       		
+        
+        	vTaskDelay(1000 / portTICK_PERIOD_MS);
+    	}
+    	
+    	
+		printf("Limpando a tela\n");
+		vTaskDelay(1000 / portTICK_PERIOD_MS);
+		vTaskDelay(1000 / portTICK_PERIOD_MS);
+		vTaskDelay(1000 / portTICK_PERIOD_MS);		
+		printf("\e[1;1H\e[2J");  // clear terminal screen.    	
+    }
+
+
+
+}
+
+
+
